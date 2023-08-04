d6892fd1aa820e0d5d9761993e420f5b38ab7da3
sofiasol1004 <137362243+sofiasol1004@users.noreply.github.com>
2023-08-04 14:47:28 +1200

    Add files via upload

---
+++ 03_not_blank.py
@@ -0,0 +1,23 @@
+# function goes here
+
+# checks that user response is not blank
+def not_blank(question):
+
+    while True:
+        response = input(question)
+
+        # if the response is blank, outputs error
+        if response == "":
+            print("Sorry this can't be blank. Please try again")
+        else:
+            return response
+
+
+#main routine goes here
+while True:
+    name = not_blank("enter your name (or 'xxx' to quit) ")
+    if name == "xxx":
+        break
+
+print("We are done")
+
