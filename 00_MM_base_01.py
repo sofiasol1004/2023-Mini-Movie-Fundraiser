3e7c79bb55bba8d515f0a68be4853c817231ce07
sofiasol1004 <137362243+sofiasol1004@users.noreply.github.com>
2023-08-09 10:42:04 +1200

    Add files via upload

--- 00_MM_base_01.py
+++ 00_MM_base_01.py
@@ -29,6 +29,19 @@ def not_blank(question):
 			return response
 
 
+# checks user enter an integer to a given question
+def num_check(question):
+
+    while True:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print("Please enter an integer.")
+
+
 # main routine starts here
 
 # set maximum number of tickets below
@@ -44,12 +57,22 @@ if want_instructions == "yes":
 print()
 
 # loop to sell tickets
-
 while tickets_sold < MAX_TICKETS:
 	name = not_blank("enter your name (or 'xxx' to quit) ")
 
 	if name == 'xxx':
 		break
+
+	age = num_check("Age: ")
+
+	if 12 <= age <= 120:
+		pass
+	elif age < 12:
+		print("Sorry you are too young for this movie")
+		continue
+	else:
+		print("?? That looks like a typo, please try again.")
+		continue
 
 	tickets_sold += 1
 
