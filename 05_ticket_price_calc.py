da203e06756b85c91ef471f6a35d5437acc93c21
sofiasol1004 <137362243+sofiasol1004@users.noreply.github.com>
2023-08-15 08:58:00 +1200

    Add files via upload

--- 05_ticket_price_calc.py
+++ 05_ticket_price_calc.py
@@ -22,3 +22,7 @@ while True:
     # Get age (assume users input a valid integer)
     age = int(input("Age: "))
 
+    # calculate ticket cost
+    ticket_cost = calc_ticket_price(age)
+    print("Age: {}, Ticket Price: ${:.2f}".format(age, ticket_cost))
+
