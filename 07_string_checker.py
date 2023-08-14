e3b9082176937663abb8edd30b1856a042fea315
sofiasol1004 <137362243+sofiasol1004@users.noreply.github.com>
2023-08-15 09:28:54 +1200

    Add files via upload

---
+++ 07_string_checker.py
@@ -0,0 +1,10 @@
+#checks that users enter a vaid response (eg yes / no
+# cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+    pass
+
+
+# main routine starts here
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
