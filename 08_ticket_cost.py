a86cba5eee678f05d3d958fc2d01dcca1844fcaf
sofiasol1004 <137362243+sofiasol1004@users.noreply.github.com>
2023-08-22 09:32:20 +1200

    Add files via upload

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,4 +1,17 @@
+# Next Steps
+# Add currency formatting & Headings
+# integrate this component with the base component
+
+# PS: In base component remember to calculate surcharge
+# once payment method has been chosen!!
+
 import pandas
+
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
 
 # dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
@@ -6,16 +19,16 @@ all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
 surcharge = [0, 0, 0.53, 0.53, 0]
 
 mini_movie_dict = {
-   "Name": all_names,
-   "Ticket Price": all_ticket_costs,
-   "Surcharge": surcharge
+    "Name": all_names,
+    "Ticket Price": all_ticket_costs,
+    "Surcharge": surcharge
 }
 
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
 # Calculate the total ticket cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] + mini_movie_frame['Ticket Price']
-
 
 # Calculate the profit for each ticket
 mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
@@ -24,8 +37,19 @@ mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
 
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("---- Ticket Data ----")
+print()
+
 # output table with ticket data
 print(mini_movie_frame)
+
+print()
+print("----- Ticket Cost / Profit -----")
 
 # output total ticket sales and profit
 print("Total Ticket Sales: ${:.2f}".format(total))
